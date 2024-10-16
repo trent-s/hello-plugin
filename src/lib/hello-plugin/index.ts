268423886b155a0fe2232ad0ef2ac8444ed9b83c
Scott Trent <trent@jp.ibm.com>
2024-10-16 14:34:49 +0900

    commit log
    Signed-off-by: Scott Trent <trent@jp.ibm.com>

--- src/lib/hello-plugin/index.ts
+++ src/lib/hello-plugin/index.ts
@@ -4,21 +4,22 @@ import {PluginParams, ConfigParams} from '@grnsft/if-core/types';
 export const HelloPlugin = PluginFactory({
   configValidation: (config: ConfigParams) => {
     // do config validation here or just pass zod schema
-    console.log("HelloPlugin: configValidation");
+    console.log('HelloPlugin: configValidation');
     return config;
   },
   inputValidation: (input: PluginParams) => {
     // do input validation here or pass zod schema
-    console.log("HelloPlugin: inputValidation");
+    console.log('HelloPlugin: inputValidation');
     return input;
   },
   implementation: async (inputs: PluginParams[], config: ConfigParams) => {
     const {helloValue} = config;
-    console.log("HelloPlugin: implementation");
+    console.log('HelloPlugin: implementation');
 
     return inputs.map(input => {
       // helloValue;
-      console.log("HelloPlugin: implementation.return");
+      console.log('HelloPlugin: implementation.return');
+      console.log(helloValue);
 
       return input;
     });
